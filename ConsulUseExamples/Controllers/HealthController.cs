b59ffd7fe9bcd2cf868eb3b13953632adec5ff88
root <hbweixiang@outlook.com>
2021-05-08 14:51:45 +0800

    init project

---
+++ ConsulUseExamples/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsulUseExamples.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            return "ok";
+        }
+    }
+}
